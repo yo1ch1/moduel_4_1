7a2830353a9507c0d1ad6a07997faf4e49db6e86
yo1ch1 <dmit.petukhov2017@gmail.com>
2024-10-01 23:36:55 +1200

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+
+
+def divide(first, second):
+   if second == 0:
+        res = ('Ошибка!')
+   else:
+       res = first/second
+   return res
---
+++ moduel_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as divide_fm
+from true_math import divide as divide_tm
+result1 = divide_fm(69, 3)
+result2 = divide_fm(3, 0)
+result3 = divide_tm(49, 7)
+result4 = divide_tm(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
